554acee0bd65b4b1d0edd971f89232ae25c3eadd
jutt975 <161073310+jutt975@users.noreply.github.com>
2024-02-26 04:11:13 +0500

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
-    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
-    return cooked;
-};
-// would yoSe a persontor’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric,u like to learn some Python today?”
-var Hello = "wahaj";
-console.log[" ", ",u like to learn some Python today?"]
