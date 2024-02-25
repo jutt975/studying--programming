cb63d950c27a37b16be1f47503c543be580b1d54
jutt975 <161073310+jutt975@users.noreply.github.com>
2024-02-26 04:10:50 +0500

    Delete app.ts

--- app.ts
+++
@@ -1,5 +0,0 @@
-
-// would yoSe a persontor’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric,u like to learn some Python today?”
-let Hello ="wahaj";
-
-console.log ` ${Hello},u like to learn some Python today?`
